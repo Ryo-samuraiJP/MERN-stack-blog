8a4dff4982a833ba5c549604347a0a495fecce6c
Rio <onlytru6340@gmail.com>
2024-10-07 14:11:41 -0700

    Add comment functionality to articles API

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -4,14 +4,17 @@ let articelsInfo = [
   {
     name: 'learn-react', 
     upvotes: 0, 
+    comment: [],
   }, 
   {
     name: 'learn-node', 
     upvotes: 0,
+    comment: [],
   }, 
   {
     name: 'mongodb', 
     upvotes: 0,
+    comment: [],
   }
 ]
 
@@ -29,6 +32,20 @@ app.put('/api/articles/:name/upvote', (req, res) => {
     res.send('The article doesn\'t exist');
   }
 
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  const article = articelsInfo.find(a => a.name === name);
+
+  if (article) {
+    article.comments.push({ postedBy, text }); 
+    res.send(article.comments);
+  } else {
+    res.send('The article doesn\'t exist!');
+  }
 });
 
 app.listen(8000, () => {

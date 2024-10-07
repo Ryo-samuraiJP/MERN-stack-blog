c4f5f6819c361fcce61d35b138c7d17d3d171232
Rio <onlytru6340@gmail.com>
2024-10-06 19:56:04 -0700

    Implement the put request for upvoting articles

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,13 +1,37 @@
 import express from 'express'; 
 
+let articelsInfo = [
+  {
+    name: 'learn-react', 
+    upvotes: 0, 
+  }, 
+  {
+    name: 'learn-node', 
+    upvotes: 0,
+  }, 
+  {
+    name: 'mongodb', 
+    upvotes: 0,
+  }
+]
+
 const app = express();
 app.use(express.json());
 
-app.post('/hello', (req, res) => {
-  res.send(`Hello ${req.body.name}`);
+app.put('/api/articles/:name/upvote', (req, res) => {
+  const { name } = req.params; 
+  const article = articelsInfo.find(a => a.name === name); 
+  
+  if (article) {
+    article.upvotes += 1;
+    res.send(`The ${name} articlce now has ${article.upvotes} upvotes`);
+  } else {
+    res.send('The article doesn\'t exist');
+  }
+
 });
 
 app.listen(8000, () => {
-  console.log('SErver is listening on port 8000');
+  console.log('Server is listening on port 8000');
 });
 

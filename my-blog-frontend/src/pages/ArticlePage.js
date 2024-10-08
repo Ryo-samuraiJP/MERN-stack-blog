04d5161359a6e28d6a6202cb6eb197dbdeff9840
Rio <onlytru6340@gmail.com>
2024-10-07 20:18:38 -0700

    Add React hooks and call it at the right time

--- my-blog-frontend/src/pages/ArticlePage.js
+++ my-blog-frontend/src/pages/ArticlePage.js
@@ -1,8 +1,15 @@
 import { useParams } from "react-router-dom";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
+import { useEffect, useState } from "react";
 
 const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    setArticleInfo({ upvotes: Math.ceil(Math.random() * 10), comments: [] });
+  }, []);
+
   const params = useParams(); 
   const articleId = params.articleId; 
   const article = articles.find(article => article.name === articleId);
@@ -14,6 +21,7 @@ const ArticlePage = () => {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This article has {articleInfo.upvotes} upvote(s)</p>
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}

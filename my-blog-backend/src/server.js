f50afc33cb4a02c2a04d89c4c8976491b790deaa
Rio <onlytru6340@gmail.com>
2024-10-07 18:50:09 -0700

    Rewrite upvote endpoint

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -21,17 +21,24 @@ app.get('/api/articles/:name', async (req, res) => {
   }
 });
 
-app.put('/api/articles/:name/upvote', (req, res) => {
+app.put('/api/articles/:name/upvote', async (req, res) => {
   const { name } = req.params; 
-  const article = articelsInfo.find(a => a.name === name); 
-  
+
+  const client = new MongoClient('mongodb://127.0.0.1:27017');
+  await client.connect();
+
+  const db = client.db('react-blog-db');
+  await db.collection('articles').updateOne({ name }, {
+    $inc: { upvotes: 1 },
+  });
+  const article = await db.collection('articles').findOne({ name });
+
   if (article) {
     article.upvotes += 1;
     res.send(`The ${name} articlce now has ${article.upvotes} upvotes!!`);
   } else {
     res.send('The article doesn\'t exist');
   }
-
 });
 
 app.post('/api/articles/:name/comments', (req, res) => {

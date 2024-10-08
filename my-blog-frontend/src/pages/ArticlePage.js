e631c68f9ef504459e2268d6285013b9cfee0105
Rio <onlytru6340@gmail.com>
2024-10-08 14:34:15 -0700

    Create the form to add comment

--- my-blog-frontend/src/pages/ArticlePage.js
+++ my-blog-frontend/src/pages/ArticlePage.js
@@ -4,6 +4,7 @@ import NotFoundPage from "./NotFoundPage";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import CommentsList from "../components/CommentsList";
+import AddCommentForm from "../components/AddCommentForm";
 
 const ArticlePage = () => {
   const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
@@ -40,6 +41,10 @@ const ArticlePage = () => {
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
+      <AddCommentForm
+        articleName={articleId}
+        onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)}
+      />
       <CommentsList comments={articleInfo.comments} />
     </>
   );  

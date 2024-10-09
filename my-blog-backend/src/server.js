1233fc29f72914e0da73f618d8bbabe29f8ce7d2
Rio <onlytru6340@gmail.com>
2024-10-09 02:12:23 -0700

    Prepare an app for release

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -2,6 +2,11 @@ import fs from 'fs';
 import admin from 'firebase-admin';
 import express from 'express';
 import { db, connectToDb } from './db.js';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const credentials = JSON.parse(
   fs.readFileSync('./credentials.json')
@@ -12,6 +17,11 @@ admin.initializeApp({
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get(/^(?!\/api).+/, (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 app.use(async (req, res, next) => {
   const { authtoken } = req.headers;
@@ -91,10 +101,12 @@ app.post('/api/articles/:name/comments', async (req, res) => {
   }
 });
 
+const PORT = process.env.PORT || 8000;
+
 connectToDb(() => {
   console.log("Successfully connected to the database!");
-  app.listen(8000, () => {
-    console.log('Server is listening on port 8000');
+  app.listen(PORT, () => {
+    console.log('Server is listening on port ' + PORT);
   });
 });
 
